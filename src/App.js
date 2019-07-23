100902469582b69dbfa1552d223cd698f47d9a25
Adriel da Silva <silvaadriel510@gmail.com>
2019-07-23 14:46:32 -0300

    calculator component created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <>
+      <h1>Calculator</h1>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import './styles.sass';
+
+export default class Calculator extends Component {
+  render() {
+    return <div className='calculator' />;
+  }
+}
