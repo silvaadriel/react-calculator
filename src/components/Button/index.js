3645b3990f3eef7b7355feca69eb44e31f64d3c2
Adriel da Silva <silvaadriel510@gmail.com>
2019-07-23 15:13:51 -0300

    button component created

---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './styles.sass';
+
+const Button = props => <button className="button">{props.btnLabel}</button>;
+
+export default Button;
--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import Button from '../Button';
 
 import './styles.sass';
 
 export default class Calculator extends Component {
   render() {
-    return <div className='calculator' />;
+    return (
+      <div className="calculator">
+        <Button btnLabel="AC" />
+        <Button btnLabel="/" />
+        <Button btnLabel="7" />
+        <Button btnLabel="8" />
+        <Button btnLabel="9" />
+        <Button btnLabel="*" />
+        <Button btnLabel="4" />
+        <Button btnLabel="5" />
+        <Button btnLabel="6" />
+        <Button btnLabel="-" />
+        <Button btnLabel="1" />
+        <Button btnLabel="2" />
+        <Button btnLabel="3" />
+        <Button btnLabel="+" />
+        <Button btnLabel="0" />
+        <Button btnLabel="." />
+        <Button btnLabel="=" />
+      </div>
+    );
   }
 }
