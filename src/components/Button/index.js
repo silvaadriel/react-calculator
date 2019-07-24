58ef92a25fd6d676ccb0eed0adfb0795f51f1019
Adriel da Silva <silvaadriel510@gmail.com>
2019-07-24 16:17:14 -0300

    implemented method handleBtnLabel and styled classes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 import './styles.sass';
 
-const Button = props => <button className="button">{props.btnLabel}</button>;
+const Button = props => {
+  let classes = 'button ';
+  classes += props.operation ? 'operation' : '';
+  classes += props.double ? 'double' : '';
+  classes += props.triple ? 'triple' : '';
+
+  const handleBtnLabel = (e) => props.click && props.click(e.target.innerHTML);
+
+  return (
+    <button className={classes} onClick={handleBtnLabel}>
+      {props.btnLabel}
+    </button>
+  );
+};
 
 export default Button;
